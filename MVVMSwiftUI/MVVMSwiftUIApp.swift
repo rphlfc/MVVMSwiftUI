0a9e1a02c12aec3c0dfbe191ed4490267e0d791c
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-10-11 21:06:00 -0300

    Initial Commit

---
+++ MVVMSwiftUI/MVVMSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMSwiftUIApp.swift
+//  MVVMSwiftUI
+//
+//  Created by Raphael Cerqueira on 11/10/20.
+//
+
+import SwiftUI
+
+@main
+struct MVVMSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
